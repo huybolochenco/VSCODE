4fed3975f81ae3f02c3f587d877ce2dfb14454f1
huybolochenco <25021539@vnu.edu.vn>
2025-11-30 21:51:12 +0700

    Bàitậptuần3

--- Untitled-4.py
+++ Untitled-4.py
@@ -1,3 +1,30 @@
 print("gayassfuck")
 
+def add(a, b):
+    return a+b
+
+result = add(5,3)
+print("Sum:", result)
+import math
+
+
+
+
+a1=b1,c1,a2,b2,a3=map(float,input().split())
+TB=((a1+b1+c1)+(a2+b2)*2+a3*3)/10
+print(round(TB,1))
+3,4,5,6,7,8
+
+
+a1, b1, c1, a2, b2, a3 = map(float, input("1:2:3:4:5:6").split())
+TB = ((a1 + b1 + c1) + (a2 + b2) * 2 + a3 * 3) / 10
+print(f"{TB:.1f}")
+1,2,3,4,5,6
+
+a1, b1, c1, a2, b2, a3 = map(float, input("Nhập 6 điểm, cách nhau bằng dấu cách: ").split())
+a1, b1, c1, a2, b2, a3 = 1, 2, 3, 4, 5, 6
+TB = ((a1 + b1 + c1) + (a2 + b2) * 2 + a3 * 3) / 10
+print(round(TB,1))
+1,2,3,4,5,6
+
 
